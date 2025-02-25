d4a56f85e4d7194bd844b4a5ea1dbde1e2e3c2e1
Chandan <Chandan17ee@gmail.com>
2025-02-25 17:11:47 +0530

    feat: integrated stepper

---
+++ src/Pages/Stepper/Components.tsx
@@ -0,0 +1,115 @@
+import styles from "./stepper.module.css";
+export const Component1 = () => {
+  return (
+    <div className="wrapper">
+      <div className="clash-card barbarian">
+        <div className="clash-card__image clash-card__image--barbarian">
+          <img
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/barbarian.png"
+            alt="barbarian"
+            loading="lazy"
+          />
+        </div>
+        <div className={styles.centerItem}>
+          <div className="clash-card__level clash-card__level--barbarian">
+            Level 1
+          </div>
+          <div className="clash-card__unit-name">The Barbarian</div>
+        </div>
+      </div>
+      {/* end clash-card barbarian*/}
+    </div>
+  );
+};
+
+export const Component2 = () => {
+  return (
+    <div className="wrapper">
+      <div className="clash-card archer">
+        <div className="clash-card__image clash-card__image--archer">
+          <img
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/archer.png"
+            alt="archer"
+            loading="lazy"
+          />
+        </div>
+        <div className={styles.centerItem}>
+          {" "}
+          <div className="clash-card__level clash-card__level--archer">
+            Level 2
+          </div>
+          <div className="clash-card__unit-name">The Archer</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const Component3 = () => {
+  return (
+    <div className="wrapper">
+      <div className="clash-card giant">
+        <div className="clash-card__image clash-card__image--giant">
+          <img
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/giant.png"
+            alt="giant"
+            loading="lazy"
+          />
+        </div>
+        <div className={styles.centerItem}>
+          <div className="clash-card__level clash-card__level--giant">
+            Level 3
+          </div>
+          <div className="clash-card__unit-name">The Giant</div>
+        </div>
+      </div>
+      {/* end clash-card giant*/}
+    </div>
+  );
+};
+
+export const Component4 = () => {
+  return (
+    <div className="wrapper">
+      <div className="clash-card goblin">
+        <div className="clash-card__image clash-card__image--goblin">
+          <img
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/goblin.png"
+            alt="goblin"
+            loading="lazy"
+          />
+        </div>
+        <div className={styles.centerItem}>
+          <div className="clash-card__level clash-card__level--goblin">
+            Level 4
+          </div>
+          <div className="clash-card__unit-name">The Goblin</div>
+        </div>
+      </div>
+      {/* end clash-card goblin*/}
+    </div>
+  );
+};
+
+export const Component5 = () => {
+  return (
+    <div className="wrapper">
+      <div className="clash-card wizard">
+        <div className="clash-card__image clash-card__image--wizard">
+          <img
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/wizard.png"
+            alt="wizard"
+            loading="lazy"
+          />
+        </div>
+        <div className={styles.centerItem}>
+          <div className="clash-card__level clash-card__level--wizard">
+            Level 5
+          </div>
+          <div className="clash-card__unit-name">The Wizard</div>
+        </div>
+      </div>
+      {/* end clash-card wizard*/}
+    </div>
+  );
+};
--- src/Pages/Stepper/Stepper.tsx
+++ src/Pages/Stepper/Stepper.tsx
@@ -1,7 +1,83 @@
-import styles from './stepper.module.css'
+import { useState } from "react";
+import styles from "./stepper.module.css";
+import {
+  Component1,
+  Component2,
+  Component3,
+  Component4,
+  Component5,
+} from "./Components";
+
+const Components = [
+  Component1,
+  Component2,
+  Component3,
+  Component4,
+  Component5,
+]
+
+const steps = [
+  "first Step",
+  "second Step",
+  "third Step",
+  "fourth Step",
+  "fifth Step",
+];
+steps.length = Components.length;
 
 const Stepper = () => {
-  return <section className={styles.container}>Stepper</section>;
+  const [activeStepper, setActiveStepper] = useState<number>(0);
+
+  const handleNext = (): void => {
+    setActiveStepper(Math.min(steps.length, activeStepper + 1));
+  };
+
+  const handleBack = (): void => {
+    setActiveStepper(Math.max(0, activeStepper - 1));
+  };
+
+  return (
+    <section className={styles.container}>
+      <ul>
+        {steps.map((step, index) => {
+          return (
+            <li key={index}>
+              <p
+                className={styles.itemIndex}
+                data-step={index <= activeStepper}
+              >
+                {index + 1}
+              </p>
+              <p className={styles.itemText}>{step}</p>
+            </li>
+          );
+        })}
+      </ul>
+
+      <div className={styles.products}>
+        {Components[activeStepper]()}
+      </div>
+
+      <div className={styles.btns}>
+        <button
+          type="button"
+          aria-label="back button"
+          onClick={handleBack}
+          disabled={!activeStepper}
+        >
+          Back
+        </button>
+        <button
+          aria-label="next button"
+          type="button"
+          onClick={handleNext}
+          disabled={activeStepper + 1 === steps.length}
+        >
+          Next
+        </button>
+      </div>
+    </section>
+  );
 };
 
 export default Stepper;
