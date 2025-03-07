2027b02fbd774525dcf73121079f8cceaccf298d
Chandan <Chandan17ee@gmail.com>
2025-03-07 23:45:44 +0530

    chore: started otp

--- src/Pages/OTP/Otp.tsx
+++ src/Pages/OTP/Otp.tsx
@@ -4,6 +4,25 @@ const TOTAL_FIELD = 4;
 const Otp = () => {
   const inputRef = useRef<(HTMLInputElement | null)[]>([]);
 
+  const handleInputEvent = (
+    event: React.KeyboardEvent<HTMLInputElement>,
+    index: number
+  ) => {
+    // console.log(event, "event",index);
+
+    if (event.code === "Space" && index + 1 < inputRef.current.length) {
+      inputRef.current[index + 1]?.focus();
+    } else if (event.code === "Backspace" && index - 1 >= 0) {
+      if (inputRef.current[index]) {
+        inputRef.current[index]!.value = "";
+      }
+      inputRef.current[index - 1]?.focus();
+    }
+  };
+
+const handlePaste = (event: React.ClipboardEvent<HTMLInputElement>) => {
+  console.log(event, 'event paste', event.clipboardData.getData('text'));
+}
   return (
     <section>
       <div className={styles.main}>
@@ -14,6 +33,15 @@ const Otp = () => {
               key={index}
               type="text"
               maxLength={1}
+              onInput={(event) => {
+                if ((event.target as HTMLInputElement).value.trim()) {
+                  inputRef.current[index + 1]?.focus();
+                }
+              }}
+              onKeyDown={(event) => {
+                handleInputEvent(event, index);
+              }}
+              onPaste={handlePaste}
             />
           );
         })}
