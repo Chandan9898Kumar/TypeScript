2f66b93fa1eab14b28a46a9cd1e973abf90942f7
Chandan <Chandan17ee@gmail.com>
2025-04-25 20:41:12 +0530

    chore: completed form

--- src/Pages/TabBasedForm/TabBasedForm.tsx
+++ src/Pages/TabBasedForm/TabBasedForm.tsx
@@ -1,4 +1,11 @@
-import { ChangeEvent, MouseEvent, useCallback, useMemo, useState } from "react";
+import {
+  ChangeEvent,
+  MouseEvent,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
 import { PageData } from "./Config";
 import Interest from "./InterestTab";
 import Public from "./PublicTab";
@@ -6,10 +13,50 @@ import Settings from "./SettingsTab";
 import TabButton from "./TabButton";
 import Tabs from "./Tabs";
 import styles from "./tab.module.css";
+
 type Tab = "Public" | "Interest" | "Setting";
 
-const Pages = [Public, Interest, Settings];
+interface BaseData {
+  id: number;
+  name: string;
+  type: string;
+}
 
+interface PublicData extends BaseData {
+  placeholder: string;
+  value: string;
+}
+
+interface InterestSettingData extends BaseData {
+  checked: boolean;
+}
+
+// Define separate props for each component type
+type ComponentProps =
+  | {
+      // type: "public";
+      data: PublicData[];
+      changeFunction: (
+        id: number,
+        event: ChangeEvent<HTMLInputElement>
+      ) => void;
+    }
+  | {
+      type: "interest" | "setting";
+      data: InterestSettingData[];
+      changeFunction: (
+        id: number,
+        event: ChangeEvent<HTMLInputElement>
+      ) => void;
+    };
+
+type TabComponent = React.ComponentType<ComponentProps>;
+
+const Pages: TabComponent[] = [
+  Public as TabComponent,
+  Interest as TabComponent,
+  Settings as TabComponent,
+];
 const TABS: Tab[] = ["Public", "Interest", "Setting"];
 
 interface Button {
@@ -19,24 +66,12 @@ interface Button {
   Submit: () => void;
 }
 
-interface PublicData {
-  id: number;
-  name: string;
-  placeholder: string;
-  value: string;
-  type: string;
-}
+type FormData = PublicData | InterestSettingData;
 
-interface InterestSettingData {
-  id: number;
-  name: string;
-  checked: boolean;
-  type: string;
-}
 interface TabsData {
   [index: string]: {
-    data: PublicData[] | InterestSettingData[];
-    setData: (id: number, event: React.ChangeEvent<HTMLInputElement>) => void;
+    data: FormData[];
+    setData: (id: number, event: ChangeEvent<HTMLInputElement>) => void;
   };
 }
 
@@ -45,42 +80,57 @@ export default function TabBasedForm() {
   const [publicData, setPublic] = useState<PublicData[]>(PageData.Public);
   const [interestData, setInterestData] = useState<InterestSettingData[]>(PageData.Interest);
   const [settingData, setSettingData] = useState<InterestSettingData[]>(PageData.Settings);
-  const [userInformation, setUserInformation] = useState({});
-
-  const Component = Pages[activeTab];
+  const [userInformation, setUserInformation] = useState<FormData[][]>([]);
 
   const TabsData: TabsData = {
     Public: {
       data: publicData,
-      setData: (id:number, event:ChangeEvent<HTMLInputElement>) => {
-        setPublic((prevData)=>{
-          return prevData?.map((item)=>{
-            return item.id === id ? {...item, value: event.target.value} : item
-          })
-        })
+      setData: (id: number, event: ChangeEvent<HTMLInputElement>) => {
+        setPublic((prevData) => {
+          return prevData.map((item) => {
+            return item.id === id
+              ? { ...item, value: event.target.value }
+              : item;
+          });
+        });
       },
     },
     Interest: {
       data: interestData,
-      setData: (id:number, event:ChangeEvent<HTMLInputElement>) => {
-        console.log("Interest", id, event.target.value);
+      setData: (id: number, event: ChangeEvent<HTMLInputElement>) => {
+        setInterestData((prevData) => {
+          return prevData.map((item) => {
+            return item.id === id
+              ? { ...item, checked: event.target.checked }
+              : item;
+          });
+        });
       },
     },
     Setting: {
       data: settingData,
-      setData: (id:number, event:ChangeEvent<HTMLInputElement>) => {
-        console.log("setting Data", id, event.target.value);
+      setData: (id: number, event: ChangeEvent<HTMLInputElement>) => {
+        setSettingData((prevData) => {
+          return prevData.map((item) => {
+            return item.id === id
+              ? { ...item, checked: event.target.checked }
+              : item;
+          });
+        });
       },
     },
   };
 
-  //  Button Component
-  const BUTTONS: Button = useMemo(() => ({
+  const BUTTONS: Button = useMemo(
+    () => ({
       Next: () => setActiveTab((prev) => prev + 1),
       Prev: () => setActiveTab((prev) => prev - 1),
       Submit: () => {
+        const PublicData = publicData.filter((item) => item.value);
+        const InterestData = interestData.filter((item) => item.checked);
+        const SettingData = settingData.filter((item) => item.checked);
         setActiveTab(0);
-        setUserInformation({ publicData, interestData, settingData });
+        setUserInformation([PublicData, InterestData, SettingData]);
         setPublic(PageData.Public);
         setInterestData(PageData.Interest);
         setSettingData(PageData.Settings);
@@ -89,7 +139,8 @@ export default function TabBasedForm() {
     [interestData, publicData, settingData]
   );
 
-  const handleClick = useCallback((event: MouseEvent<HTMLButtonElement>) => {
+  const handleClick = useCallback(
+    (event: MouseEvent<HTMLButtonElement>) => {
       const button = BUTTONS[(event.target as HTMLButtonElement).value];
       if (button) {
         button();
@@ -98,6 +149,41 @@ export default function TabBasedForm() {
     [BUTTONS]
   );
 
+  const SubmittedData = !!userInformation.length && (
+    <div className={styles["submitted-data"]}>
+      {userInformation.flat().map((data) => (
+        <div key={data.id}>
+          {data.name} : {"  "}
+          {"value" in data ? data.value : data.checked.toString()}
+        </div>
+      ))}
+    </div>
+  );
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+    if (userInformation.length) {
+      timer = setTimeout(() => {
+        setUserInformation([]);
+      }, 5000);
+    }
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [userInformation]);
+
+  const renderActiveComponent = () => {
+    const Component = Pages[activeTab];
+    const currentTab = TABS[activeTab];
+
+    return (
+      <Component
+        data={TabsData[currentTab].data as PublicData[] & InterestSettingData[]}
+        changeFunction={TabsData[currentTab].setData}
+      />
+    );
+  };
 
   return (
     <div className={styles.formContainer}>
@@ -105,12 +191,7 @@ export default function TabBasedForm() {
       <div className={styles.tabsContainer}>
         <Tabs tabs={TABS} activeTab={activeTab} />
       </div>
-      <div className={styles.componentContainer}>
-        <Component
-          data={TabsData[TABS[activeTab]].data}
-          changeFunction={TabsData[TABS[activeTab]].setData}
-        />
-      </div>
+      <div className={styles.componentContainer}>{renderActiveComponent()}</div>
       <div className={styles.buttonContainer}>
         <TabButton
           activeTab={activeTab}
@@ -118,6 +199,7 @@ export default function TabBasedForm() {
           onClick={handleClick}
         />
       </div>
+      <div>{SubmittedData}</div>
     </div>
   );
 }
