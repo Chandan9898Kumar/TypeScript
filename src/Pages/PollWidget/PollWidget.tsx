3fe8627ae72ff5140337f1ef1476a804e9be5143
Chandan Kumar <chandan17ee@gmail.com>
2025-06-01 21:43:10 +0530

    fix: fixed polling

--- src/Pages/PollWidget/PollWidget.tsx
+++ src/Pages/PollWidget/PollWidget.tsx
@@ -2,8 +2,21 @@ const PollWidget = () => {
   return (
     <div>
       <h1 style={{color:'gray'}}>Poll Widget</h1>
+      <Polling />
     </div>
   );
 };
 
 export default PollWidget;
+
+
+
+
+
+const Polling=()=>{
+  return (
+    <div>
+    </div>
+  );
+
+}
