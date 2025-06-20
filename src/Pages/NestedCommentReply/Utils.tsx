e2634b2ec6c50293112dca9716e2af9582ce958c
Chandan Kumar <chandan17ee@gmail.com>
2025-06-20 19:53:19 +0530

    fix(Utils): fixed function return type

--- src/Pages/NestedCommentReply/Utils.tsx
+++ src/Pages/NestedCommentReply/Utils.tsx
@@ -11,7 +11,9 @@ export const getUniqueId = ():number => {
 };
 
 export const getDate = () => {
+  
   const now = new Date();
+
   const formattedDate = `${now.getFullYear()}-${(now.getMonth() + 1)
     .toString()
     .padStart(2, "0")}-${now.getDate().toString().padStart(2, "0")} ${now
